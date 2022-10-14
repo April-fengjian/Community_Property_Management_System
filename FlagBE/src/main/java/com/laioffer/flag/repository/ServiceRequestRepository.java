76df5d57127588e3dedfa976b70fd82f2b83eb0e
BTian8 <boyan.t@wustl.edu>
2022-10-14 01:09:03 -0500

    ServiceRequestRepository update

--- FlagBE/src/main/java/com/laioffer/flag/repository/ServiceRequestRepository.java
+++ FlagBE/src/main/java/com/laioffer/flag/repository/ServiceRequestRepository.java
@@ -3,9 +3,10 @@ package com.laioffer.flag.repository;
 import com.laioffer.flag.model.ServiceRequest;
 import com.laioffer.flag.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ServiceRequestRepository extends JpaRepository<ServiceRequest, Long> {
     List<ServiceRequest> findByHost(User user);
     ServiceRequest findByIdAndHost(Long id, User host);
