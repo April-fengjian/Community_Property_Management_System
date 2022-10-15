e9be9b1f221f17870280c1a0d5d481ed7d11ca99
LuliaZhang <Lulia_123@outlook.com>
2022-10-15 08:54:43 -0700

    update messageUtils

--- flagfe/src/utils/messageUtils.js
+++ flagfe/src/utils/messageUtils.js
@@ -1,16 +1,51 @@
-  // this function is to get all messages
-  export const getMessages = () => {
+// this function is to get all messages
+export const getAllMessages = () => {
     const authToken = localStorage.getItem("authToken"); // can get userID
     const listMessagesUrl = `${domain}/messages`;
 
     return fetch(listMessagesUrl, {
-      headers: {
+        headers: {
         Authorization: `Bearer ${authToken}`,
-      },
+        },
     }).then((response) => {
-      if(response.status !== 200){
+        if(response.status !== 200){
         throw Error("Fail to get message list");
-      }
-      return response.json();
+        }
+        return response.json();
     });
-  };
+};
+
+// this function is used to post messages
+export const postMessage = (data) => {
+    const authToken = localStorage.getItem("authToken");
+    const postMessageUrl = `${domain}/messages`;
+
+    return fetch(postMessageUrl, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        },
+        body: data,
+    }).then((response) => {
+        if(response.status !== 200){
+            throw Error("Fail to post message");
+        }
+    });
+};
+
+// this function is used to delete the message
+export const deleteMessage = (messageId) => {
+    const authToken = localStorage.getItem("authToken");
+    const deleteMessageUrl = `${domain}/messages/${messageId}`;
+
+    return fetch(deleteMessageUrl, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        },
+    }).then((response) => {
+        if(response.status !== 200){
+            throw Error("Fail to delete message");
+        }
+    });
+};
