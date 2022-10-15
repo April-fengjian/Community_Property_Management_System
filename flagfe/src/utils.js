3f036d27cfd17f2361950fc836462a20f24c5331
LuliaZhang <Lulia_123@outlook.com>
2022-10-15 08:37:30 -0700

    added frontend utils

--- flagfe/src/utils.js
+++ flagfe/src/utils.js
@@ -30,3 +30,20 @@ export const register = (credential, asManager) => {
       }
     });
   };
+
+  // this function is to get all messages
+  export const getMessages = () => {
+    const authToken = localStorage.getItem("authToken"); // can get userID
+    const listMessagesUrl = `${domain}/messages`;
+
+    return fetch(listMessagesUrl, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }).then((response) => {
+      if(response.status !== 200){
+        throw Error("Fail to get message list");
+      }
+      return response.json();
+    });
+  };
